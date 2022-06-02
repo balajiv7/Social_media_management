69df6f9537e7c62b79c73a2a3950644d5fead613
Balaji V <bala3010@global.chainsys.com>
2022-06-02 20:25:34 +0530

    include node

--- server/nanodb.js
+++ server/nanodb.js
@@ -1,7 +1,7 @@
 const nano = require('nano');
 const url = "https://apikey-v2-380rhhqzbqk6eifbn30gvuevzk9903pdrrsd7f8ipknj:ee0e39016c30dc0fc4fd04d12a420174@5804af1c-53d6-4cc1-b0eb-5219a1cc5775-bluemix.cloudant.com";
-const nanodb = nano(process.env.COUCHDB_URL || url);// connect with couchdb
-const trainee = nanodb.use('balaji_trainee'); //connect with database 
+const nanodb = nano(process.env.COUCHDB_URL || url);
+const trainee = nanodb.use('balaji_trainee'); 
 
 const nodemail = require('nodemailer');
 
